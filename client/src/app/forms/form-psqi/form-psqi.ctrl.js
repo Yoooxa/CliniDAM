2e0bff0654327cc083ea56169dd24fcd1f71b653
ArthurLnq <arthur.lannelucq&outlook.fr>
2016-12-12 23:22:36 +0100

    WIP : form psqi

--- client/src/app/forms/form-psqi/form-psqi.ctrl.js
+++ client/src/app/forms/form-psqi/form-psqi.ctrl.js
@@ -5,10 +5,28 @@
    * @name  formPsqiCtrl
    * @description Controller
    */
-  function FormPsqiCtrl($log) {
+  function FormPsqiCtrl($scope) {
 
+    var vm = this;
+
+    vm.cardNumber = 1
+
+    $scope.log = function() {
+      console.log('--- vm ---')
+      console.log(vm)
+    }
+
+    $scope.setNextCardNumber = function() {
+      vm.cardNumber++
+    }
+
+    $scope.setPreviousCardNumber = function() {
+      vm.cardNumber--
+    }
   }
 
   angular.module('form-psqi', [])
-    .controller('FormPsqiCtrl', FormPsqiCtrl);
+    .controller('FormPsqiCtrl', FormPsqiCtrl)
+
+    .$inject = ['$scope'];
 })();

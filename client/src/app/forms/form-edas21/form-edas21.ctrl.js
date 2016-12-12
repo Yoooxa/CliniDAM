3c3b64f528781099890ebb530a07ea484f7965ba
ArthurLnq <arthur.lannelucq&outlook.fr>
2016-12-12 17:09:55 +0100

    Add form edas21 in project

---
+++ client/src/app/forms/form-edas21/form-edas21.ctrl.js
@@ -0,0 +1,17 @@
+/**
+ * Created by arthur on 12/12/2016.
+ */
+(function() {
+  'use strict';
+
+  /**
+   * @name  formEdas21Ctrl
+   * @description Controller
+   */
+  function FormEdas21Ctrl($log) {
+
+  }
+
+  angular.module('form-edas21', [])
+    .controller('FormEdas21Ctrl', FormEdas21Ctrl);
+})();

2fb1de8af9c8d4d4822edd1c4abce5846b303cd8
lesdsch <lea.deschamps@ynov.com>
2016-12-12 17:22:31 +0100

    WIP Form ISI

--- client/src/app/forms/form-isi/form-isi.ctrl.js
+++ client/src/app/forms/form-isi/form-isi.ctrl.js
@@ -5,10 +5,16 @@
    * @name  formIsiCtrl
    * @description Controller
    */
-  function FormIsiCtrl($log) {
-
+  function FormIsiCtrl($scope) {
+    
+    $scope.data = {};
+    $scope.data.cb1 = true;
+    $scope.data.cb2 = false;
+    $scope.data.cb3 = false;
+    $scope.data.cb4 = false;
+    $scope.data.cb5 = false;
   }
 
-  angular.module('form-isi', [])
+  angular.module('form-isi', ['ngMaterial'])
     .controller('FormIsiCtrl', FormIsiCtrl);
 })();

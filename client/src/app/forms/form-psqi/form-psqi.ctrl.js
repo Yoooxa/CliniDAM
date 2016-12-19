9e9efd52aa10187cc8949404f496f49699c38667
ArthurLnq <arthur.lannelucq&outlook.fr>
2016-12-19 22:58:57 +0100

    Add form-psqi view + controller

--- client/src/app/forms/form-psqi/form-psqi.ctrl.js
+++ client/src/app/forms/form-psqi/form-psqi.ctrl.js
@@ -9,6 +9,13 @@
 
     var vm = this;
 
+    vm.inputsGroup = [
+      {label: "Pas au cours du dernier mois", value: 1},
+      {label: "Moins d'une fois par semaine", value: 2},
+      {label: "1 à 2 fois par semaine", value: 3},
+      {label: "3 ou 4 fois par semaine", value: 4}
+    ]
+
     vm.cardNumber = 1
 
     $scope.log = function() {
@@ -28,5 +35,5 @@
   angular.module('form-psqi', [])
     .controller('FormPsqiCtrl', FormPsqiCtrl)
 
-    .$inject = ['$scope'];
+    .$inject = ['$scope']
 })();
